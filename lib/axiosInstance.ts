60ecea7b249cde836101168be7c383adda941f6a
Zainab Kassim Temitope <kassimzainab11@gmail.com>
2026-07-08 21:18:05 -0400

    fix(auth):fix: proxy API requests through Next.js rewrites to fix cross-site cookie issue on iOS

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useCurrencyStore } from '@/store/currencyStore';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: '/api',
   withCredentials: true,
 });
 
